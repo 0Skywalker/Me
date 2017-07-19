1ff5756d34ecad996f1837bb2180b6dedf507791
JediAlejandro <alejandrocg1015@gmail.com>
2017-07-19 09:17:57 -0700

    Adding transition page rippler material design

--- js/animation.js
+++ js/animation.js
@@ -33,7 +33,7 @@ $(function() {
      ripple_wrap.removeClass('goripple');
    });
 
-   $('body').on('click', 'td a', function(e) {
+   $('body').on('click', '#projects', function(e) {
      rippler.css('left', e.clientX + 'px');
      rippler.css('top', e.clientY + 'px');
      e.preventDefault();
