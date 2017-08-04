f7745a5d0fa4f00c2eae86b263b08c22fb4133bf
JediAlejandro <alejandrocg1015@gmail.com>
2017-08-03 22:32:50 -0700

    Adding projects

--- js/animation.js
+++ js/animation.js
@@ -13,7 +13,7 @@ $(document).ready(function() {
       monitor_1 = function(el_1) {
         var computed_1 = window.getComputedStyle(el_1, null),
             borderwidth_1 = parseFloat(computed_1.getPropertyValue('border-left-width'));
-        if (!finish_1 && borderwidth_1 >= 1000) {
+        if (!finish_1 && borderwidth_1 >= 950) {
           el_1.style.WebkitAnimationPlayState = "paused";
           el_1.style.animationPlayState = "paused";
           swapContent_1();
@@ -119,7 +119,7 @@ $(document).ready(function() {
       monitor_3 = function(el_3) {
         var computed_3 = window.getComputedStyle(el_3, null),
             borderwidth_3 = parseFloat(computed_3.getPropertyValue('border-left-width'));
-        if (!finish_3 && borderwidth_3 >= 1000) {
+        if (!finish_3 && borderwidth_3 >= 950) {
           el_3.style.WebkitAnimationPlayState = "paused";
           el_3.style.animationPlayState = "paused";
           swapContent_3();
