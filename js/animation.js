7b19d1d2ae16394e0761dc6de92217eb763e8e2c
JediAlejandro <alejandrocg1015@gmail.com>
2017-08-07 23:53:27 -0700

    adding ripple effect to card

--- js/animation.js
+++ js/animation.js
@@ -7,7 +7,18 @@ $(function() {
 
 /* Card */
 
-
+$(document).ready(function() {
+  $('body').on('touchstart click', '#findFace', function(e) {
+       var ripple = $(this).find(".ripple");
+       ripple.removeClass("animate");
+       var x = parseInt(e.pageX - $(this).offset().left) - (ripple.width() / 2);
+       var y = parseInt(e.pageY - $(this).offset().top) - (ripple.height() / 2);
+       ripple.css({
+          top: y,
+          left: x
+       }).addClass("animate");
+  });
+});
 
 /*
 $('body').on('touchstart click', '#findFace', function(e) {
