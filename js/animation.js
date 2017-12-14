5f9594d0ea1da9d7f5c2941c99440c287f739617
JediAlejandro <alejandrocg1015@gmail.com>
2017-12-13 21:38:04 -0800

    cleaning div-extract

--- js/animation.js
+++ js/animation.js
@@ -4,17 +4,3 @@ $(document).ready(function(){
       $("#p_email").fadeToggle(300);
     });
 });
-
-/* Material Design Ripple - Cards */
-$(document).ready(function() {
-  $('body').on('click touchstart', '.card', function(e) {
-       var ripple = $(this).find(".ripple");
-       ripple.removeClass("animate");
-       var x = parseInt(e.pageX - $(this).offset().left) - (ripple.width() / 2);
-       var y = parseInt(e.pageY - $(this).offset().top) - (ripple.height() / 2);
-       ripple.css({
-          top: y,
-          left: x
-       }).addClass("animate");
-  });
-});
