d435d514eb6a40658f7fc010d414434b52dcc50c
JediAlejandro <alejandrocg1015@gmail.com>
2018-03-11 08:22:19 -0700

    Adding links

--- js/animation.js
+++ js/animation.js
@@ -1,1 +1,1 @@
-$(document).ready(function(){$("#ic_email").click(function(){$("#p_email").fadeToggle(300)})})
+$(document).ready(function(){$("#ic_email").click(function(){$("#p_email").fadeToggle(300)})})document.addEventListener("touchstart", function(){}, true);
